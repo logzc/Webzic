8e2046ebe27e80a0331bb477e004bf94df0f26b3
Zic <logzcx@126.com>
2016-09-05 10:47:32 +0800

    Refine the Modules.

--- fork/src/main/java/com/lish/fork/hashcode/Main.java
+++
@@ -1,7 +0,0 @@
-package com.lish.fork.hashcode;
-
-/**
- * Created by lishuang on 2016/7/19.
- */
-public class Main {
-}
--- fork/src/test/resources/sql/customer_create.sql
+++
@@ -1,2 +0,0 @@
-drop table if exists `customer` ;
-CREATE TABLE `customer` (  `id` int(11) primary key NOT NULL AUTO_INCREMENT,  `name` varchar(255) not null,  `contact` varchar(255) not null,  `telephone` varchar(255),  `email` varchar(255),  `remark` text) ENGINE=InnoDB DEFAULT CHARSET=utf8;
